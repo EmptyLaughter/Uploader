587ac98604840395e6df233349abb6d42e39bc57
Christine <justpeachiii@yahoo.com>
2017-01-17 21:44:39 -0800

    Changed variable 'dir' to 'direc' or 'directory' since 'dir' is a predfined variable. Changed input() to raw_input().

--- Main.py
+++ Main.py
@@ -46,7 +46,7 @@ def deleteFile(file_path):
 
 def writeTo(log_file_path, details):
     """
-    @summary: srites the details of a successful upload and deletion to a log file;
+    @summary: writes the details of a successful upload and deletion to a log file;
     a new log file will be created if the given one does not already exist
     @param log_file_path: path to the log file
     @param details: date and time of successful upload and path of deleted filed
@@ -56,59 +56,54 @@ def writeTo(log_file_path, details):
     f.write(details + "\n")
     f.close()
 
-def monitorFile(dir_to_watch, log_file, upload_dest):
+def monitorFile(dir_to_watch, log_file, dbx_auth_token, dbx_file_path):
     """
     @summary: watches a configurable directory, uploads the file to specified online account,
     and deletes the file on local hard disk upon successful upload
     @param dir_to_watch: path to directory
     @param log_file: log file to write timestamps and details to
-    @param upload_dest: online account (i.e. dropbox)
+    @param dbx_auth_token: account authorization token
+    @param dbx_file_path: configurable path to file on Dropbox
     """
-    dir = dir_to_watch
+    direc = dir_to_watch
     log = log_file
-    dest = upload_dest
-
-    #checks if dropbox
-    if "box" in dest:
-        print "Enter token authentication key:"
-        dbx_auth_token = str(input())
-        print "Enter the configurable path to the file on your Dropbox account:"
-        dbx_file_path = str(input())
-        print "Now monitoring... \n"
+    auth_token = dbx_auth_token
+    file_path = dbx_file_path
 
     #size and time new modifications are checked against
-    last_modified_file = fileSize(dir)
+    last_modified_file = fileSize(direc)
     now_timestamp = time.asctime(time.localtime(time.time()))
     writeTo(log, "Start: " + str(now_timestamp))
     now_timestamp_datetime = datetime.strptime(now_timestamp, "%a %b %d %H:%M:%S %Y")
 
     while True:
-        new_modified_file = fileSize(dir)
+        new_modified_file = fileSize(direc)
         if last_modified_file != new_modified_file:
-            for f in os.listdir(dir):
-                path = os.path.join(dir, f)
+            for f in os.listdir(direc):
+                path = os.path.join(direc, f)
                 f_timestamp = time.ctime(os.stat(path).st_ctime)  # time file was created
                 f_timestamp_datetime = datetime.strptime(f_timestamp, "%a %b %d %H:%M:%S %Y")  # creates datetime object of time created
                 if f_timestamp_datetime > now_timestamp_datetime:
                     writeTo(log, f)
-                    if "box" in dest:
-                        file_content = open(path)
-                        file_content_tostring = str(file_content.read())
-                        upload = dropboxUpload(dbx_auth_token, file_content_tostring, dbx_file_path + "-" + str(f))
-                        writeTo(log, upload)
-                        if "Successful" in upload:
-                            delete = deleteFile(path)
-                            writeTo(log, delete)
-                            new_modified_file = fileSize(dir)
+                    file_content = open(path)
+                    file_content_tostring = str(file_content.read())
+                    upload = dropboxUpload(auth_token, file_content_tostring, file_path + "-" + str(f))
+                    writeTo(log, upload)
+                    if "Successful" in upload:
+                        delete = deleteFile(path)
+                        writeTo(log, delete)
 
 print "Enter the directory path: "
-dir = str(input())
+directory = str(raw_input())
 print "Enter the log file path: "
-log = str(input())
-print "Enter the storage account you want to upload to: (i.e. Dropbox): "
-dest = str(input())
+log = str(raw_input())
+print "Enter token authentication key:"
+dbx_auth_token = str(raw_input())
+print "Enter the configurable path to the file on your Dropbox account:"
+dbx_file_path = str(raw_input())
+print "Now monitoring... \n"
 
-monitorFile(dir, log, dest)
+monitorFile(directory, log, dbx_auth_token, dbx_file_path)
 
 
 
